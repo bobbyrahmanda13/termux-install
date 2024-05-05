75de45c43d81b86006c30579805e4bc8b1807ff9
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-05 14:45:12 +0700

    add zoxide

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkg install -y neovim git curl nodejs-lts python-pip aria2 wget termux-api build-essential fd unzip ripgrep renameutils libtiff libjpeg-turbo libxcb libraqm libimagequant openjpeg littlecms ndk-sysroot clang make x11-repo fzf tmux golang
+pkg install -y neovim git curl nodejs-lts python-pip aria2 wget termux-api build-essential fd unzip ripgrep renameutils libtiff libjpeg-turbo libxcb libraqm libimagequant openjpeg littlecms ndk-sysroot clang make x11-repo fzf tmux golang zoxide
 pip install yt-dlp qrcode pillow
 termux-setup-storage
 npm install -g nodemon tree-sitter-cli
