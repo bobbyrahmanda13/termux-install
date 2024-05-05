bfcf69aeb551348ad936517d50ace4d0bc99a89d
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-05 12:51:45 +0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pkg install -y neovim git curl nodejs-lts python-pip aria2 wget termux-api build-essential fd unzip ripgrep renameutils libtiff libjpeg-turbo libxcb libraqm libimagequant openjpeg littlecms ndk-sysroot clang make
+pip install yt-dlp qrcode pillow
