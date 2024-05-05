f9dda4fcab7bae0016d0241ad89723bd19665b14
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-05 13:18:12 +0700

    add fzf and tmux

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pkg install -y neovim git curl nodejs-lts python-pip aria2 wget termux-api build-essential fd unzip ripgrep renameutils libtiff libjpeg-turbo libxcb libraqm libimagequant openjpeg littlecms ndk-sysroot clang make x11-repo
+pkg install -y neovim git curl nodejs-lts python-pip aria2 wget termux-api build-essential fd unzip ripgrep renameutils libtiff libjpeg-turbo libxcb libraqm libimagequant openjpeg littlecms ndk-sysroot clang make x11-repo fzf tmux
 pip install yt-dlp qrcode pillow
 termux-setup-storage
