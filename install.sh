ca82e23954cb66c4862b42b58841b445e1c69349
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-05-05 13:13:10 +0700

    add termux-setup-storage

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pkg install -y neovim git curl nodejs-lts python-pip aria2 wget termux-api build-essential fd unzip ripgrep renameutils libtiff libjpeg-turbo libxcb libraqm libimagequant openjpeg littlecms ndk-sysroot clang make
+pkg install -y neovim git curl nodejs-lts python-pip aria2 wget termux-api build-essential fd unzip ripgrep renameutils libtiff libjpeg-turbo libxcb libraqm libimagequant openjpeg littlecms ndk-sysroot clang make x11-repo
 pip install yt-dlp qrcode pillow
+termux-setup-storage
